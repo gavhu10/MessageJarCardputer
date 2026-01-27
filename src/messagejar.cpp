8d3545d878c59da6d8027d2e2a3d22296336b2b5
gavhu10 <186771458+gavhu10@users.noreply.github.com>
2026-01-26 20:30:29 -0500

    Some message sending functinality added

--- src/messagejar.cpp
+++ src/messagejar.cpp
@@ -1,19 +1,19 @@
+#include "messagejar.h"
+
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-#include <string>
-using std::string;
-
 #include <memory>
+#include <string>
+#include <vector>
+#include <map>
+
 using std::make_shared;
 using std::shared_ptr;
+using std::string;
 using std::unique_ptr;
-
-#include <vector>
 using std::vector;
-
-#include <map>
 
 #define SERVER_URL "https://gavhu10.pythonanywhere.com/api/"
 
@@ -40,141 +40,120 @@ unique_ptr<string> request(const string &endpoint, const std::map<string, string
 
     http.end();
     return nullptr;
-};
+}
 
-class Message
+Message::Message(const std::map<string, string> &data)
 {
-public:
-    Message(const std::map<string, string> &data)
+    try
     {
-        try
-        {
-            author = data.at("author");
-            content = data.at("content");
-            timestamp = data.at("timestamp");
-            message_id = data.at("message_id");
-        }
-        catch (const std::out_of_range &)
-        {
-            author = "";
-            content = "error loading message";
-            timestamp = "";
-            message_id = "";
-        }
-    };
-
-    string as_string() const
+        author = data.at("author");
+        content = data.at("content");
+        timestamp = data.at("timestamp");
+        message_id = data.at("message_id");
+    }
+    catch (const std::out_of_range &)
     {
-        return "[" + timestamp + "] " + author + ": " + content;
-    };
+        author = "";
+        content = "error loading message";
+        timestamp = "";
+        message_id = "";
+    }
+}
 
-private:
-    string author;
-    string content;
-    string timestamp;
-    string message_id;
-};
-
-class MessageJar
+string Message::as_string() const
 {
-public:
-    MessageJar(string username, string password) : username(username), password(password) {};
+    return "[" + timestamp + "] " + author + ": " + content;
+}
 
-    bool check()
+MessageJar::MessageJar(string username, string password) : username(username), password(password) {}
+
+bool MessageJar::check()
+{
+    auto response = request("/api-manage", {{"username", username}, {"password", password}, {"action", "verify_user"}});
+    return response != nullptr;
+}
+
+bool MessageJar::create_user()
+{
+    auto response = request("/api-manage", {{"username", username}, {"password", password}, {"action", "new_user"}});
+    return response != nullptr;
+}
+
+shared_ptr<vector<string>> MessageJar::get_rooms()
+{
+    if (!check())
     {
-        auto response = request("/api-manage", {{"username", username}, {"password", password}, {"action", "verify_user"}});
-        return response != nullptr;
-    };
+        return nullptr;
+    }
 
-    bool create_user()
+    auto response = request("/api-manage", {{"username", username}, {"password", password}, {"action", "list_rooms"}});
+    if (!response)
     {
-        auto response = request("/api-manage", {{"username", username}, {"password", password}, {"action", "new_user"}});
-        return response != nullptr;
-    };
+        return nullptr;
+    }
 
-    shared_ptr<vector<string>> get_rooms()
+    auto rooms = make_shared<vector<string>>();
+    JsonDocument doc;
+    deserializeJson(doc, response->c_str());
+    JsonArray arr = doc.as<JsonArray>();
+    for (JsonVariant v : arr)
     {
-        if (!check())
-        {
-            return nullptr;
-        }
+        rooms->push_back(v.as<string>());
+    }
 
-        auto response = request("/api-manage", {{"username", username}, {"password", password}, {"action", "list_rooms"}});
-        if (!response)
-        {
-            return nullptr;
-        }
+    return rooms;
+}
 
-        // Parse response and create room list
-        auto rooms = make_shared<vector<string>>();
-
-        JsonDocument doc;
-        deserializeJson(doc, response->c_str());
-        JsonArray arr = doc.as<JsonArray>();
-        for (JsonVariant v : arr)
-        {
-            rooms->push_back(v.as<string>());
-        }
-
-        return rooms;
-    };
-
-    shared_ptr<vector<Message>> get_messages(string room, int latest = 0)
+shared_ptr<vector<Message>> MessageJar::get_messages(string room, int latest)
+{
+    if (!check())
     {
-        if (!check())
-        {
-            return nullptr;
-        }
+        return nullptr;
+    }
 
-        auto response = request("/get_messages", {{"username", username}, {"password", password}, {"room", room}});
-        if (!response)
-        {
-            return nullptr;
-        }
-
-        // Parse response and create Message objects
-        auto messages = make_shared<vector<Message>>();
-        JsonDocument doc;
-        deserializeJson(doc, response->c_str());
-        JsonArray arr = doc.as<JsonArray>();
-        for (JsonVariant v : arr)
-        {
-            std::map<string, string> msg_data;
-            JsonDocument msg_doc;
-            deserializeJson(msg_doc, v.as<string>().c_str());
-            for (auto pair : msg_doc.as<JsonObject>())
-            {
-                msg_data[pair.key().c_str()] = pair.value().as<string>();
-            }
-            messages->push_back(Message(msg_data));
-        }
-
-        return messages;
-    };
-
-    bool send(string room, string content)
+    auto response = request("/get_messages", {{"username", username}, {"password", password}, {"room", room}});
+    if (!response)
     {
-        if (!check())
-        {
-            return false;
-        }
+        return nullptr;
+    }
 
-        auto response = request("/api-send", {{"username", username}, {"password", password}, {"room", room}, {"message", content}});
-        return response != nullptr;
-    };
-
-    bool create_room(string room_name)
+    auto messages = make_shared<vector<Message>>();
+    JsonDocument doc;
+    deserializeJson(doc, response->c_str());
+    JsonArray arr = doc.as<JsonArray>();
+    for (JsonVariant v : arr)
     {
-        if (!check())
+        std::map<string, string> msg_data;
+        JsonDocument msg_doc;
+        deserializeJson(msg_doc, v.as<string>().c_str());
+        for (auto pair : msg_doc.as<JsonObject>())
         {
-            return false;
+            msg_data[pair.key().c_str()] = pair.value().as<string>();
         }
+        messages->push_back(Message(msg_data));
+    }
 
-        auto response = request("/api-manage", {{"username", username}, {"password", password}, {"action", "create_room"}, {"room", room_name}});
-        return response != nullptr;
-    };
+    return messages;
+}
 
-private:
-    string username;
-    string password;
-};
+bool MessageJar::send(string room, string content)
+{
+    if (!check())
+    {
+        return false;
+    }
+
+    auto response = request("/api-send", {{"username", username}, {"password", password}, {"room", room}, {"message", content}});
+    return response != nullptr;
+}
+
+bool MessageJar::create_room(string room_name)
+{
+    if (!check())
+    {
+        return false;
+    }
+
+    auto response = request("/api-manage", {{"username", username}, {"password", password}, {"action", "create_room"}, {"room", room_name}});
+    return response != nullptr;
+}
